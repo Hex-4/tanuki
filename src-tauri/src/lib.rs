b545c5088d7ee8d18232c6d6f3aa9e7c9d4f4d75
hex4 <121138331+Hex-4@users.noreply.github.com>
2025-10-05 18:17:12 -0600

    initial commit, start project and start top bar

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,24 @@
+use tauri::{
+  Manager,
+  image::Image,
+  tray::{TrayIconBuilder},
+  menu::{Menu, MenuItem},
+};
+
+
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("hello, {}! you've been greeted from rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import '../app.css';
+	
+	let { children } = $props();
+
+	import { TrayIcon, type TrayIconOptions } from '@tauri-apps/api/tray';
+	import { defaultWindowIcon } from '@tauri-apps/api/app';
+
+	import { onMount } from 'svelte';
+
+
+</script>
+
+<main class="container font-seurat bg-orange-100 text-stone-900 w-full h-full">
+  	
+	<nav class="bg-orange-200 w-full p-1 px-2 flex flex-row wavy-bottom pb-6 cursor justify-between align-middle" data-tauri-drag-region>
+		<p class="text-2xl cursor-default" data-tauri-drag-region>tanuki</p>
+		<div>
+			<button class="hover:scale-125 transition" aria-label="Minimize to tray">
+				<img src="/icons/down-caret.svg" class="size-8 inline" alt="">
+			</button>
+			<button class="hover:scale-125 transition" aria-label="Close app">
+				<img src="/icons/view-close.svg" class="size-8 inline" alt="">
+			</button>
+
+		</div>
+		
+	</nav>
+
+	{@render children()}
+</main>
+
+<style>
+	.wavy-bottom {
+  --mask:
+    radial-gradient(35.78px at 50% calc(100% - 48px),#000 99%,#0000 101%) calc(50% - 32px) 0/64px 100%,
+    radial-gradient(35.78px at 50% calc(100% + 32px),#0000 99%,#000 101%) 50% calc(100% - 16px)/64px 100% repeat-x;
+  -webkit-mask: var(--mask);
+          mask: var(--mask);
+}
+</style>
+
+
